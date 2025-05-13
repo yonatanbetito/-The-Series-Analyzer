54e4807ddd1e8419fea2223114e1a5cedc6e99af
yonatan_betito <yonatanbetito@gmail.com>
2025-05-13 10:19:20 +0300

    12_05

--- -The-Series-Analyzer/Program.cs
+++ -The-Series-Analyzer/Program.cs
@@ -1,4 +1,197 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("hi");
+﻿using System;
+using System.Globalization;
+using System.Linq;
 
+class Program
+{
+    //משתנה שמחזיק את הסדרה שלי
+    static int[] series;
+    
+    //פונקציה שבעצם תנהל את כל הפרוייקט
+    static void Main(string[] args)
+    {
+        series = get_series_from_user();
+        menu_show();
+        Manager_by_choice();
+        
+    }
+    
+    //קבלת הסדרה עי המשתמש באינפוט או בארגומנט(יבוצע בהמשך)
+    static int[] get_series_from_user()
+    {
+        int[] series;
+        do
+        {
+            Console.WriteLine("Please enter a series of numbers separated by space (at least 3 numbers): ");
+            series = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        } while (series.Length < 3);
+        foreach (int number in series)
+        {
+            Console.WriteLine(number);
+        }
+
+        return series;
+    }
+    
+    //פונקציה שמציגה את התפריט
+    static void menu_show()
+    {
+        Console.WriteLine("menu:");
+        Console.WriteLine("a. Input a Series. (Replace the current series)");
+        Console.WriteLine("b. Display the series in the order it was entered.");
+        Console.WriteLine("c. Display the series in the reversed order it was entered.");
+        Console.WriteLine("d. Display the series in sorted order (from low to high).");
+        Console.WriteLine("e. Display the Max value of the series.");
+        Console.WriteLine("f. Display the Min value of the series.");
+        Console.WriteLine("g. Display the Average of the series.");
+        Console.WriteLine("h. Display the Number of elements in the series.");
+        Console.WriteLine("i. Display the Sum of the series.");
+        Console.WriteLine("j. Exit.");
+    }
+
+    //פונקציה שמקבלת/מחזירה בחירה של המשתמש
+    static string input_choice()
+    {
+        Console.WriteLine("enter your choice(a-j): ");
+        string choice = Console.ReadLine();
+        return choice;
+    }
+
+    //פונקציה שמריצה/קוראת פונקציה אחרת בהתאם לבחירה
+     static void Manager_by_choice()
+     {
+         string choice;
+         do
+         {
+             choice = input_choice();
+             switch (choice)
+             {
+                 case "a":
+                     print_input(input_new_series());
+                     break;
+                 case "b":
+                     print_input(show_series());
+                     break;
+                 case "c":
+                     print_input(show_series_reversed());
+                     break;
+                 case "d":
+                     print_input(show_series_sorted());
+                     break;
+                 case "e":
+                     print_input(max_value());
+                     break;
+                 case "f":
+                     print_input(min_value());
+                     break;
+                 case "g":
+                     print_input(average_value());
+                     break;
+                 case "h":
+                     print_input(number_of_elements());
+                     break;
+                 case "i":
+                     print_input(sum_of_elements());
+                     break;
+                 case "j":
+                     Console.WriteLine("exit");
+                     break;
+                    
+                 default:
+                     Console.WriteLine("invalid choice");
+                     break;
+             }
+         } while (choice != "j");
+         
+    }
+
+     //הכנסת סדרה חדשה
+    static int[] input_new_series()
+    {
+        return series = get_series_from_user();
+    }
+
+    //הצגת הסדרה
+    static int[] show_series()
+    {
+        return series;
+    }
+
+    //פונקציה שמחזירה *רשימה חדשה* הפוכה 
+    static List<int> show_series_reversed()
+    {
+        List<int> reversed_series = new List<int>();
+        for (int index = series.Length-1; index>=0; index--)
+        {
+            reversed_series.Add(series[index]);
+        }
+
+        return reversed_series;
+    }
+
+    
+    static int[] show_series_sorted()
+    {
+        throw new NotImplementedException();
+    }
+
+    static int max_value()
+    {
+        throw new NotImplementedException();
+    }
+
+    static int min_value()
+    {
+        throw new NotImplementedException();
+    }
+
+    //מחזירה ממוצע של כל הסדרה
+    static double average_value()
+    {
+        double average;
+        int sum_of = 0;
+        foreach (int number in series)
+        {
+            sum_of += number;
+        }
+        average = sum_of / series.Length;
+        return average;
+    }
+
+    //מספר של אלמנטים
+    static int number_of_elements()
+    {
+        return series.Length;
+    }
+
+    //סכום כל האלמנטים
+    static int sum_of_elements()
+    {
+        int sum = 0;
+        foreach (int number in series)
+        {
+            sum += number;
+        }
+        return sum;
+    }
+    
+    //מה שיחזור מהפונקציה שהמנגר החזירה יודפס לפי הטייפ שלו
+    static void print_input(object input)
+    {
+        switch (input)
+        {
+            case int number:
+                Console.WriteLine($"Single number: {number}");
+                break;
+
+            case List<int> list:
+                Console.WriteLine("List of numbers: " + string.Join(" ", list));
+                break;
+
+            case int[] array:
+                Console.WriteLine("Array of numbers: " + string.Join(" ", array));
+                break;
+        }
+    }
+    
+}
